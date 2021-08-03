7a27a474ebf39a4fe80ed8c43190018f541d3ddb
Inder sein <indersein416@gmail.com>
2021-08-03 11:14:47 +0530

    update config file

--- application/config/config.php
+++ application/config/config.php
@@ -29,9 +29,7 @@ if($_SERVER['HTTP_HOST']=="localhost")
 {
   $config['base_url'] = 'http://localhost/dot-com-labs/creak/hms/smart_hospital_src/';
 }else{
-  $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-  $domainName = $_SERVER['HTTP_HOST'] . '/';
-  $config['base_url'] = $protocol . $domainName;
+  $config['base_url'] = 'https://drstorks.com/';
 }
 
 /*
